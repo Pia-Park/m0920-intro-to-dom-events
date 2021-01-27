007af91c419f45e91ecfe0feb04142776d5610e5
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-01-27 08:55:02 -0800

    teacher's solution

--- 00-exercise/app.js
+++ 00-exercise/app.js
@@ -9,3 +9,28 @@ form.addEventListener('submit', function(e){
     tweetList.style.listStyle = 'none';
     tweets.appendChild(tweetList);
 });
+
+
+//teacher's solution
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const userNameInput = form.getElementsByClassName.username;
+    const tweetInput = form.getElementsByClassName.tweet;
+    addTweet(userNameInput.value, tweetInput.value);
+    userNameInput.value = '';
+    tweetInput.value = '';
+});
+
+const addTweet = (username, tweet) => {
+    //li
+    const newTweet = document.createElement(li);
+    //b
+    const bTag = document.createElement('b');
+    //append
+    bTag.append(username);
+    newTweet.append(bTag);
+    newTweet.append(`-${tweet}`);
+    // tweetContainer.appendChild(newTweet);
+
+}
